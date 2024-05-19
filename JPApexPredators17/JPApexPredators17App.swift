35f31fc3cb67470fb83df8bf0060fb08a2a400a8
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-05-19 00:06:54 -0300

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Clovis Carmezini on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
